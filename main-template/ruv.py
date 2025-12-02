6bc647f90f9d9a297f4d81495a5629490589ad9e
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-12-02 09:26:09 -0500

    Update ruv.py

--- main-template/ruv.py
+++ main-template/ruv.py
@@ -5,7 +5,7 @@ for rep in range(nreps):
 	rep_plus_one = rep + 1
 
 	# Extract rank
-	fn = 'rep%d/smc/height_of_first_split.txt' % rep_plus_one
+	fn = 'rep%d/smc/rank_first_split.txt' % rep_plus_one
 	stuff = open(fn, 'r').read()
 	m = re.search(r'rank: (?P<rank>[.0-9]+)', stuff, re.M | re.S)
 	rank = float(m.group('rank'))
