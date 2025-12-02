2e987d18b81124abe601f045f8e7ca94458efdac
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-12-02 09:10:09 -0500

    Create coverage.py

---
+++ main-template/coverage.py
@@ -0,0 +1,36 @@
+import sys, re, os, subprocess as sub
+
+nreps = __NREPS__
+print("min:")
+
+for rep in range(nreps):
+	rep_plus_one = rep + 1
+
+	# Extract rank
+	fn = 'rep%d/smc/hpd.txt' % rep_plus_one
+	stuff = open(fn, 'r').read()
+	m1 = re.search(r'([.0-9]+)', stuff, re.M | re.S)
+	min = m1.group(0)
+	print(min, " ,")
+
+print("max:")
+for rep in range(nreps):
+        rep_plus_one = rep + 1
+
+       	# Extract rank
+        fn = 'rep%d/smc/hpd.txt' % rep_plus_one
+        stuff = open(fn, 'r').read()
+        m2 = re.search(r'\t([.0-9]+)', stuff, re.M | re.S)
+        max = m2.group(0)
+        print(max, " ,")
+        
+print("true:")
+for rep in range(nreps):
+        rep_plus_one = rep + 1
+
+       	# Extract rank
+        fn = 'rep%d/smc/hpd.txt' % rep_plus_one
+        stuff = open(fn, 'r').read()
+        m2 = re.search(r'\t([.0-9]+)\t([.0-9]+)', stuff, re.M | re.S)
+        true = m2.group(2)
+        print(true, " ,")
